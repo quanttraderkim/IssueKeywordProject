21cb4a36000571cd24e1eca6745b975893be7247
Daehwan(Daewhan) Kim <quanttraderkim@gmail.com>
2021-03-19 23:45:33 +0900

    크롤링 자동화까지 완성
    일정 횟수를 일정 시간 간격으로 크롤링하는 코드까지 완성

--- zumCrawling.py
+++ zumCrawling.py
@@ -1,6 +1,4 @@
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support.ui import Select
 import time
 
 # 크롬 드라이버로 크롬 실행
@@ -12,42 +10,35 @@ count = 1000
 pause = 3
 
 print("+" * 100)
-print("실시간 이슈 크롤링 {}회_{}마다 - 출처 : ZUM".format(count,pause))
+print("실시간 이슈 크롤링 {}회_{}마다 - 출처 : ZUM".format(count, pause))
 print("-" * 100)
 
 j = 0
 errorList = []
 while j < count:
-    try:
-        # 반복 시작
-        print("반복시작 : {}회 / {}회".format(j + 1, count))
-        # 기준 시간 크롤링
-        timeNow = driver.find_element_by_xpath('//*[@id="issue_time"]').text
 
-        # 실시간 이슈 크롤링
+    # 반복 시작
+    print("반복시작 : {}회 / {}회".format(j + 1, count))
+    # 기준 시간 크롤링
+    timeNow = driver.find_element_by_xpath('//*[@id="issue_time"]').text
 
-        # 우선 이슈키워드들을 리스트에 담고
-        issueKeywordLs = []
-        for i in list(range(1, 11)):
-            selector = '//*[@id="issue_wrap"]/ol/li[{}]/div/div[2]/a/span[2]'.format(i)
-            issueKeyword = driver.find_element_by_xpath(selector)
-            issueKeywordLs.append(issueKeyword)
+    # 우선 이슈키워드들을 리스트에 담고
+    issueKeywordLs = []
+    for i in list(range(1, 11)):
+        selector = '//*[@id="issue_wrap"]/ol/li[{}]/div/div[2]/a/span[2]'.format(i)
+        issueKeyword = driver.find_element_by_xpath(selector)
+        issueKeywordLs.append(issueKeyword)
 
-        # print(issueKeywordLs)
+    # 텍스트만 추출하여 다시 리스트에 담아둠
+    issueKeywordTextLs = []
+    for keyword in issueKeywordLs:
+        issueKeywordTextLs.append(keyword.text)
 
-        # 텍스트만 추출하여 다시 리스트에 담아둠
-        issueKeywordTextLs = []
-        for keyword in issueKeywordLs:
-            issueKeywordTextLs.append(keyword.text)
-
-        # 현재시간과 검색어 순위와 함께 검색어 텍스트 출력함
-        print('현재시간 : {}'.format(timeNow))
-        for i in list(range(1, 11)):
-            if issueKeywordTextLs[i - 1] != '':
-                print('{}. {}'.format(i, issueKeywordTextLs[i - 1]))
-
-    except:
-        errorList.append(j)
+    # 현재시간과 검색어 순위와 함께 검색어 텍스트 출력함
+    print('현재시간 : {}'.format(timeNow))
+    for i in list(range(1, 11)):
+        if issueKeywordTextLs[i - 1] != '':
+            print('{}. {}'.format(i, issueKeywordTextLs[i - 1]))
 
     time.sleep(pause)
     j = j + 1
@@ -57,5 +48,3 @@ print(errorList)
 
 # 드라이버 종료함
 driver.quit()
-
-
